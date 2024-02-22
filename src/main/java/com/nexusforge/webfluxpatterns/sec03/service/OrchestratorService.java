c99db2bfce117c15ad9d3a5ba15546dfb5b809eb
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-02-22 14:41:44 +0100

    04 - Implmentation of the Orchestration Pattern for payment Validation of a shopping applicatin  - Implementation of the OrchestratorService

---
+++ src/main/java/com/nexusforge/webfluxpatterns/sec03/service/OrchestratorService.java
@@ -0,0 +1,58 @@
+package com.nexusforge.webfluxpatterns.sec03.service;
+
+import com.nexusforge.webfluxpatterns.sec03.client.ProductClient;
+import com.nexusforge.webfluxpatterns.sec03.dto.*;
+import com.nexusforge.webfluxpatterns.sec03.util.DebugUtil;
+import com.nexusforge.webfluxpatterns.sec03.util.OrchestrationUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class OrchestratorService {
+    @Autowired
+    private ProductClient productClient;
+    @Autowired
+    private OrderFulfillmentService fulfillmentService;
+    @Autowired
+    private OrderCancellationService cancellationService;
+
+    public Mono<OrderResponse> placeOrder(Mono<OrderRequest> mono) {
+        return mono.map(OrchestrationRequestContext::new)
+                .flatMap(this::getProduct)
+                .doOnNext(OrchestrationUtil::buildRequestContext)
+                .flatMap(fulfillmentService::placeOrder)
+                .doOnNext(this::doOrderPostProcessing)
+                .doOnNext(DebugUtil::print) // debug
+                .map(this::toOrderResponse);
+    }
+
+    private void doOrderPostProcessing(OrchestrationRequestContext context) {
+        if (Status.FAILED.equals(context.getStatus())) {
+            this.cancellationService.cancelOrder(context);
+        }
+        // if the order is a success, perform post processing l
+        // like sending notifications and bla bla bla
+    }
+
+    private Mono<OrchestrationRequestContext> getProduct(OrchestrationRequestContext ctx) {
+        return this.productClient.getProduct(ctx.getOrderRequest().getProductId())
+                .map(Product::getPrice)
+                .doOnNext(ctx::setProductPrice)
+                .thenReturn(ctx);
+    }
+
+    private OrderResponse toOrderResponse(OrchestrationRequestContext ctx) {
+        var isSuccess = Status.SUCCESS.equals(ctx.getStatus());
+        var address = isSuccess ? ctx.getShippingResponse().getAddress() : null;
+        var dateDelivery = isSuccess ? ctx.getShippingResponse().getExpectedDelivery() : null;
+        return OrderResponse.create(
+                ctx.getOrderRequest().getUserId(),
+                ctx.getOrderRequest().getProductId(),
+                ctx.getOrderId(),
+                ctx.getStatus(),
+                address,
+                dateDelivery
+        );
+    }
+}
---
+++ src/main/java/com/nexusforge/webfluxpatterns/sec03/util/DebugUtil.java
@@ -0,0 +1,17 @@
+package com.nexusforge.webfluxpatterns.sec03.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nexusforge.webfluxpatterns.sec03.dto.OrchestrationRequestContext;
+
+public class DebugUtil {
+    public static void print(OrchestrationRequestContext ctx) {
+        ObjectMapper mapper = new ObjectMapper();
+        String s = null;
+        try {
+            System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(ctx));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
