16641ad54f32078a19cc3867a33c28e0062eccf1
Nurudeen Abdulmalik <Abdulmaliknurudeen4@gmail.com>
2024-02-24 15:48:20 +0100

    05 -  Implementation of Orchestrator Pattern(Sequential) for Chained Workflow. 1. Implementation of Chained WorkFlow, Part 2

--- src/main/java/com/nexusforge/webfluxpatterns/sec04/service/OrderFulfillmentService.java
+++ src/main/java/com/nexusforge/webfluxpatterns/sec04/service/OrderFulfillmentService.java
@@ -1,30 +1,43 @@
 package com.nexusforge.webfluxpatterns.sec04.service;
 
+import com.nexusforge.webfluxpatterns.sec04.client.ProductClient;
 import com.nexusforge.webfluxpatterns.sec04.dto.OrchestrationRequestContext;
+import com.nexusforge.webfluxpatterns.sec04.dto.Product;
 import com.nexusforge.webfluxpatterns.sec04.dto.Status;
+import com.nexusforge.webfluxpatterns.sec04.util.OrchestrationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
 @Service
 public class OrderFulfillmentService {
     @Autowired
-    private List<Orchestrator> orchestrators;
+    private ProductClient productClient;
+    @Autowired
+    private PaymentOrchestrator paymentOrchestrator;
+    @Autowired
+    private InventoryOrchestrator inventoryOrchestrator;
+    @Autowired
+    private ShippingOrchestrator shippingOrchestrator;
+
 
     public Mono<OrchestrationRequestContext> placeOrder(OrchestrationRequestContext ctx) {
-        var list = orchestrators.stream()
-                .map(o -> o.create(ctx))
-                .toList();
-        return Mono.zip(list, a -> a[0])
-                .cast(OrchestrationRequestContext.class)
-                .doOnNext(this::updateStatus);
+        return this.getProduct(ctx)
+                .doOnNext(OrchestrationUtil::buildPaymentRequest)
+                .flatMap(this.paymentOrchestrator::create)
+                .doOnNext(OrchestrationUtil::buildInventoryRequest)
+                .flatMap(this.inventoryOrchestrator::create)
+                .doOnNext(OrchestrationUtil::buildShipppingRequest)
+                .flatMap(this.shippingOrchestrator::create)
+                .doOnNext(c -> c.setStatus(Status.SUCCESS))
+                .doOnError(ex -> ctx.setStatus(Status.FAILED))
+                .onErrorReturn(ctx);
     }
 
-    private void updateStatus(OrchestrationRequestContext ctx) {
-        var allSuccess = this.orchestrators.stream().allMatch(o -> o.isSuccess().test(ctx));
-        var status = allSuccess ? Status.SUCCESS : Status.FAILED;
-        ctx.setStatus(status);
+    private Mono<OrchestrationRequestContext> getProduct(OrchestrationRequestContext ctx) {
+        return this.productClient.getProduct(ctx.getOrderRequest().getProductId())
+                .map(Product::getPrice)
+                .doOnNext(ctx::setProductPrice)
+                .map(i -> ctx);
     }
 }
